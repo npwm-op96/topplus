f1436af1e097196698908b28198b5ebb9c5a8b1a
nattapon wong <nattapon-w@outlook.com>
2022-10-15 14:50:44 +0700

    Fix Deploy

--- plugins/v-calendar.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import Vcalendar from 'v-calendar'
-// import 'v-calendar/lib/v-calendar.min.css';
-Vue.use(Vcalendar, {             // second is optional
-  datePickerTintColor: '#F00',
-  datePickerShowDayPopover: false
-})
